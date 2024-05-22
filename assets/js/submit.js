559917190090e49ff00c11202e432f6c9189830c
gian.gg <80165986+spookyexe@users.noreply.github.com>
2024-05-23 00:24:58 +0800

    assets for dummy QR-AMS website

---
+++ assets/js/submit.js
@@ -0,0 +1,4 @@
+function handleSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission
+    window.location.href = "pages/submit.html"; // Redirect to the specified page
+}
